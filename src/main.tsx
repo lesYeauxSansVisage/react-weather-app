264ac40ceb3506b1a00c8cf2b8bd30d40c57d981
Gen <genesishawkeye42@gmail.com>
2023-10-10 19:04:49 -0300

    FEAT: Added Context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+import { WeatherContextProvider } from "./store/weather-context.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <WeatherContextProvider>
     <App />
-  </React.StrictMode>
+  </WeatherContextProvider>
 );
