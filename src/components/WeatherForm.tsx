52843e87eb4b7293d8507106f9cb1bfdffa66bec
Gen <genesishawkeye42@gmail.com>
2023-10-18 11:42:43 -0300

    FEAT: Added WeatherContext to form

--- src/components/WeatherForm.tsx
+++ src/components/WeatherForm.tsx
@@ -1,15 +1,33 @@
+import WeatherContext from "../store/weather-context";
 import classes from "./WeatherForm.module.scss";
+import { useContext, useRef } from "react";
 
 const WeatherForm = () => {
-  const submitHandler = (event: React.FormEvent) => {
+  const { getForecast } = useContext(WeatherContext);
+  const cityRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    console.log("submitted");
+    const cityInputValue = cityRef.current!.value;
+
+    if (cityInputValue.trim() === "") {
+      console.log("Por favor, insira uma cidade");
+      return;
+    }
+
+    getForecast(cityInputValue);
+
+    event.currentTarget.reset();
   };
 
   return (
     <form className={classes["weather-form"]} onSubmit={submitHandler}>
-      <input type="text" placeholder="Insira aqui o nome da cidade" />
+      <input
+        type="text"
+        placeholder="Insira aqui o nome da cidade"
+        ref={cityRef}
+      />
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
