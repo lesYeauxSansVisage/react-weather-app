0104027a8aa95e7598de503b0247a01884f92982
Gen <genesishawkeye42@gmail.com>
2023-10-20 19:38:45 -0300

    REFACTOR: Removed context from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import { WeatherContextProvider } from "./store/weather-context.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <WeatherContextProvider>
-    <App />
-  </WeatherContextProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
