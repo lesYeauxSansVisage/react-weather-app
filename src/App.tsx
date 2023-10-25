3999b7bf6dad1f4f72afb7266ca1c455e82fab55
Gen <genesishawkeye42@gmail.com>
2023-10-25 18:48:06 -0300

    FEAT: Added WeatherContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.scss";
+import CapitalWeathers from "./components/Weathers/CapitalWeathers/CapitalWeathers";
+import MainHeader from "./components/UI/MainHeader";
+import WeatherForm from "./components/Weathers/WeatherForm";
+import WeatherInfo from "./components/Weathers/WeatherInfo/WeatherInfo";
+import { WeatherContextProvider } from "./store/weather-context";
 
 function App() {
-  return <></>
+  return (
+    <div className="container">
+      <WeatherContextProvider>
+        <MainHeader />
+        <WeatherInfo />
+        <WeatherForm />
+        <CapitalWeathers />
+      </WeatherContextProvider>
+    </div>
+  );
 }
 
 export default App;
