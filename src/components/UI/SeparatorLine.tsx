e3100842ba9cb0001cda0ac97bee7abf33403667
Gen <genesishawkeye42@gmail.com>
2024-05-07 11:55:21 -0300

    FEAT: Added SeparatorLine component

---
+++ src/components/UI/SeparatorLine.tsx
@@ -0,0 +1,8 @@
+import classes from "./SeparatorLine.module.scss";
+
+const SeparatorLine = () => {
+  return (
+    <div className={classes["separator-line"]}></div>
+  );
+};
+export default SeparatorLine;
