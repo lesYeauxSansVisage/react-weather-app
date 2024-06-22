e2d27e5980c05199f41460d82e7b18c41a5dda6c
Gen <genesishawkeye42@gmail.com>
2024-06-21 22:17:38 -0300

    FEAT: Added aria-label to loading spinner

--- src/components/UI/LoadingSpinner.tsx
+++ src/components/UI/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import classes from "./LoadingSpinner.module.scss";
 
 const LoadingSpinner = () => {
-  return <div className={classes.spinner}></div>;
+  return <div className={classes.spinner} aria-label="loading-spinner"></div>;
 };
 
 export default LoadingSpinner;
