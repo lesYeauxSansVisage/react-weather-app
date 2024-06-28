d0b8246cf30854649689e1f8488f0ce64da24830
Gen <genesishawkeye42@gmail.com>
2024-06-28 11:05:53 -0300

    CONFIG: Removed css preprocessor option

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sass from "sass";
+// import sass from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
-      },
-    },
-  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       implementation: sass,
+  //     },
+  //   },
+  // },
   base: "/weather-app/",
 });
